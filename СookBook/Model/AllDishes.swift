2f2bf69efc315610e1b7966d75692f35723329f1
Nikita <111679856+wildwoodB@users.noreply.github.comborisov.nee@icloud.comborisov.nee@icloud.comborisov.nee@icloud.comborisov.nee@icloud.comborisov.nee@icloud.comborisov.nee@icloud.comborisov.nee@icloud.com>
2022-11-30 23:30:07 +0700

    add new files

---
+++ СookBook/Model/AllDishes.swift
@@ -0,0 +1,13 @@
+//
+//  DishModel.swift
+//  ApiTest
+//
+//  Created by Админ on 29.11.2022.
+//
+
+import Foundation
+
+struct AllDishes: Decodable {
+    //let recipes: [Dish]?
+    let extendedIngredients: [Ingridients]?
+}
